257ab93a7096bfe2fcd4c2f3a56ae98fd5fbd502
silambarasan-mani <sanpri4.ss@gmail.com>
2021-05-21 19:41:59 +0530

    added some line in starter class

--- src/main/java/in/simbu/SpringBootAppGitApplication.java
+++ src/main/java/in/simbu/SpringBootAppGitApplication.java
@@ -2,12 +2,17 @@ package in.simbu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import in.simbu.beans.Car;
 
 @SpringBootApplication
 public class SpringBootAppGitApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAppGitApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(SpringBootAppGitApplication.class, args);
+		context.getBean(Car.class);
+		
 	}
 
 }
--- src/main/java/in/simbu/beans/Car.java
+++ src/main/java/in/simbu/beans/Car.java
@@ -1,5 +1,8 @@
 package in.simbu.beans;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Car {
 	
 	public void getCar()
