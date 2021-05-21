498adf5f4e93ab3f326f72fe7107ebf2f47410e6
silambarasan-mani <sanpri4.ss@gmail.com>
2021-05-21 19:16:41 +0530

    intital project commit

---
+++ src/main/java/in/simbu/SpringBootAppGitApplication.java
@@ -0,0 +1,13 @@
+package in.simbu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppGitApplication.class, args);
+	}
+
+}
